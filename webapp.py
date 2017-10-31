4ddacd826135d0d6038d7af12ae5afedcb5514d2
kebudke <31251507+kebudke@users.noreply.github.com>
2017-10-31 09:59:39 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -1,7 +1,16 @@
 from flask import Flask, url_for, render_template, request
+import json
 
-app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
+app = Flask(__name__)
 
 @app.route("/")
 def render_main():
     return render_template('home.html')
+
+def get_state_options(counties):
+    for state in counties:
+        
+    options += Markup("<option value=\"" + s + "\">" + s + "</option>")
+    
+if __name__=="__main__":
+    app.run(debug=False, port=54321)
