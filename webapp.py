d8df4969f1c4717261190db96d3371ae98b1b6b6
kebudke <31251507+kebudke@users.noreply.github.com>
2017-11-16 09:34:33 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -16,13 +16,13 @@ def get_state_options():
         options += Markup("<option value=\"" + o + "\">" + o + "</option>")
     return options
 
-def get_fun_fact():
-    fact = request.args['state']
+def get_fun_fact(state):
+    fact = state
     return fact
 
 @app.route("/")
 def render_main():
-    return render_template('home.html', options = get_state_options(), funfact = get_fun_fact())
+    return render_template('home.html', options = get_state_options(), funfact = get_fun_fact(request.args['state']))
 
 if __name__=="__main__":
     app.run(debug=False, port=54321)
