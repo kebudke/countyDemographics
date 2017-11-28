035132cc47db9b01e46977e199c7b99c1e662d57
kebudke <31251507+kebudke@users.noreply.github.com>
2017-11-28 09:32:03 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -29,7 +29,7 @@ def get_fun_fact(state):
             totalpop[s["State"]] = s["Population"]["2014 Population"]
     for b in counties:
         if b["State"] in popwithb:
-            popwithb[b["State"]] = popwithb[s["State"]] + ((b["Education"]["Bachelor's Degree or Higher"])*.01)*(b["Population"]["2014 Population"])
+            popwithb[b["State"]] = popwithb[b["State"]] + ((b["Education"]["Bachelor's Degree or Higher"])*.01)*(b["Population"]["2014 Population"])
         else:
             popwithb[b["State"]] = ((b["Education"]["Bachelor's Degree or Higher"])*.01)*(b["Population"]["2014 Population"])
     for p in counties:
